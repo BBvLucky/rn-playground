524986112ecda721499a0d0182269afe1ce75602
Irakli <bbvlucky@gmail.com>
2026-07-02 13:17:29 +0300

    rename project

--- android/app/src/main/java/com/playground/MainApplication.kt
+++ android/app/src/main/java/com/playground/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sometestpetproj
+package com.playground
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/playground/AppDelegate.swift
+++ ios/playground/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "SomeTestPetProj",
+      withModuleName: "playground",
       in: window,
       launchOptions: launchOptions
     )
